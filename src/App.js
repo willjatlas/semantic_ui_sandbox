fc17457af0aeabd2758e049873b35011b1320707
William Anderson <willjatlas@googlemail.com>
2021-02-18 21:21:16 +0000

    sliders work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import Searchbar from './components/Searchbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Searchbar />
     </>
   );
 }
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,7 +1,36 @@
+import {Slider} from "react-semantic-ui-range"
+import {Label, Segment} from "semantic-ui-react"
+import {useState} from 'react';
+
 const Searchbar = () => {
 
-    return(
+    const [value, setValue] = useState(6);
 
+    const settings = {
+        start : 5,
+        min   : 0,
+        max   : 10,
+        step  : 1,
+        onChange : value => {
+            setValue(value);
+        }
+    }
+
+    const handleValueChange = (event) => {
+        let value = parseInt(event.target.value);
+        if(!value){
+            value = 0;
+        }
+        setValue(event.target.value);
+    }
+
+    return(
+        <div id="slider-wrap">
+        <Segment size="small">
+            <Slider value={value} settings={settings} /> 
+        </Segment>
+        <Label color="red">{value}</Label>
+        </div>
     )
 
 } 
